092e602783ad5c2cecca53456a2ff5d63744d480
Bahy <Homelander.SolderBoy@gmail.com>
2023-06-27 19:21:18 +0300

    Update GUC.py
    removed unnecessary comments

--- GUC.py
+++ GUC.py
@@ -2,8 +2,6 @@ import webbrowser
 import threading
 from dotenv import load_dotenv
 import os
-
-
 load_dotenv()
 
 
@@ -16,18 +14,14 @@ mail = 'https://mail.guc.edu.eg/owa/'
 cms = 'https://cms.guc.edu.eg/apps/student/HomePageStn.aspx'
 portal = 'https://apps.guc.edu.eg/student_ext/Console.aspx'
 
-# create three threads, each of which opens a different URL in a new tab
 t1 = threading.Thread(target=open_url, args=(mail,))
 t2 = threading.Thread(target=open_url, args=(cms,))
 t3 = threading.Thread(target=open_url, args=(portal,))
 
-# start the threads
 t1.start()
 t2.start()
 t3.start()
 
-# wait for the threads to finish
 t1.join()
 t2.join()
 t3.join()
-
